552acabd6fa605091ac0db5be2f74bfbc5352444
broadwells <broadwell.s@gmail.com>
2017-05-23 12:02:11 -0400

    Comparsion Tiles

--- src/main/java/com/grandcircus/spring/controller/HomeController.java
+++ src/main/java/com/grandcircus/spring/controller/HomeController.java
@@ -3,19 +3,17 @@ package com.grandcircus.spring.controller;
 import com.uber.sdk.rides.client.ServerTokenSession;
 import com.uber.sdk.rides.client.SessionConfiguration;
 import com.uber.sdk.rides.client.UberRidesApi;
-
 import com.uber.sdk.rides.client.model.*;
 import com.uber.sdk.rides.client.services.RidesService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 import retrofit2.Response;
 
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
+
 
 /**
  * Created by broadwells on 5/22/17.
@@ -25,10 +23,13 @@ import java.util.List;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public String helloworld(Model model){
+    @RequestMapping(value = "/")
+    public String helloworld(Model model) {
         List<Product> results;
         List<PriceEstimate> prices;
+        List<TimeEstimate> duration;
+        String id = "";
+
         try {
             SessionConfiguration config = new SessionConfiguration.Builder()
                     .setClientId("clientId")
@@ -39,32 +40,50 @@ public class HomeController {
 
             UberRidesApi ride = UberRidesApi.with(session).build();
             RidesService service = ride.createService();
+            //product
             Response<ProductsResponse> response = service.getProducts(42.335734f, -83.050031f).execute();
             ProductsResponse products = response.body();
             results = products.getProducts();
 
+            //price
             Response<PriceEstimatesResponse> respond = service.getPriceEstimates(42.335734f, -83.050031f,
-                    42.352450f, -83.061618f).execute();
+                    42.462633f, -82.891155f).execute();
             PriceEstimatesResponse priceTag = respond.body();
             prices = priceTag.getPrices();
 
-            model.addAttribute("price", prices);
-            model.addAttribute("product", results);
 
-//            for (PriceEstimate i: prices) {
-//
-//                System.out.println(i.getEstimate());
-//            }
-//            for (Product j : results){
-//                System.out.println(j.getDisplayName());
-//            }
+            //time
+            Response<TimeEstimatesResponse> responseTime = service.getPickupTimeEstimate(42.335734f, -83.050031f,
+                    id).execute();
 
-        }catch (IOException e){
-            
+            TimeEstimatesResponse time = responseTime.body();
+
+            duration = time.getTimes();
+
+            String displayName = results.get(0).getDisplayName();
+            String discript = results.get(0).getDescription();
+            int cap = results.get(0).getCapacity();
+
+            String priceEst = prices.get(0).getEstimate() + " " + prices.get(0).getCurrencyCode();
+            Float distance = prices.get(0).getDistance();
+
+            int seconds = duration.get(0).getEstimate();
+            int eta = (seconds%3600)/60;
+
+            model.addAttribute("product", displayName);
+            model.addAttribute("descrip", discript);
+            model.addAttribute("cap", cap);
+            model.addAttribute("price", priceEst);
+            model.addAttribute("mile", distance);
+            model.addAttribute("time", eta);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
 
         return "welcome";
-    }
 
+    }
 }
